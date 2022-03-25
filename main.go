fdb6378cebe556a1cdf226f6e50790825a8d4efc
Ruairi Callaghan <ruairiscallaghan@gmail.com>
2022-03-25 09:24:13 +0000

    Initial auth working, with CLI flags

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/FuzzyStatic/blizzard/v3"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	client_id := flag.String("id", "", "Battle.net Client ID")
+	client_secret := flag.String("secret", "", "Battle.net Client Secret")
+	flag.Parse()
+
+	ctx := context.Background()
+	blizzClient, client_err := blizzard.NewClient(blizzard.Config{
+		ClientID:     *client_id,
+		ClientSecret: *client_secret,
+		HTTPClient:   http.DefaultClient,
+		Region:       blizzard.EU,
+		Locale:       blizzard.EnGB,
+	})
+	if client_err != nil {
+		fmt.Println(client_err)
+	}
+
+	token_err := blizzClient.AccessTokenRequest(ctx)
+	if token_err != nil {
+		fmt.Println(token_err)
+	}
+
+	data, _, request_err := blizzClient.WoWCharacterProfileSummary(ctx, "outland", "puzzle")
+	if request_err != nil {
+		fmt.Println("Error querying API", request_err)
+	}
+
+	fmt.Printf("%+v\n", data)
 }

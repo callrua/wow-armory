69c1e44a1f29a8007ab36c49c15d919dd80007b0
Ruairi Callaghan <rcallaghan@c02gf1lmmd6n.lan>
2022-04-06 10:37:39 +0100

    Use go standard for vars

--- main.go
+++ main.go
@@ -4,36 +4,37 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"net/http"
-
 	"github.com/FuzzyStatic/blizzard/v3"
+	"github.com/callrua/wow-armory/utility"
+	"net/http"
 )
 
 func main() {
-	client_id := flag.String("id", "", "Battle.net Client ID")
-	client_secret := flag.String("secret", "", "Battle.net Client Secret")
+	utility.GetOauth("hi", "you")
+	clientId := flag.String("id", "", "Battle.net Client ID")
+	clientSecret := flag.String("secret", "", "Battle.net Client Secret")
 	flag.Parse()
 
 	ctx := context.Background()
-	blizzClient, client_err := blizzard.NewClient(blizzard.Config{
-		ClientID:     *client_id,
-		ClientSecret: *client_secret,
+	blizzClient, clientErr := blizzard.NewClient(blizzard.Config{
+		ClientID:     *clientId,
+		ClientSecret: *clientSecret,
 		HTTPClient:   http.DefaultClient,
 		Region:       blizzard.EU,
 		Locale:       blizzard.EnGB,
 	})
-	if client_err != nil {
-		fmt.Println(client_err)
+	if clientErr != nil {
+		fmt.Println(clientErr)
 	}
 
-	token_err := blizzClient.AccessTokenRequest(ctx)
-	if token_err != nil {
-		fmt.Println(token_err)
+	tokenErr := blizzClient.AccessTokenRequest(ctx)
+	if tokenErr != nil {
+		fmt.Println(tokenErr)
 	}
 
-	data, _, request_err := blizzClient.WoWCharacterProfileSummary(ctx, "outland", "puzzle")
-	if request_err != nil {
-		fmt.Println("Error querying API", request_err)
+	data, _, requestErr := blizzClient.WoWCharacterProfileSummary(ctx, "outland", "puzzle")
+	if requestErr != nil {
+		fmt.Println("Error querying API", requestErr)
 	}
 
 	fmt.Printf("%+v\n", data)

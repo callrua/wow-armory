9ce5e1b8bb5ab52bd1af4067c5fed0afca95ca20
Ruairi Callaghan <ruairiscallaghan@gmail.com>
2022-04-07 10:06:39 +0100

    Implements cobra functionality, moves get player functionality out to it's own cobra command

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"github.com/callrua/wow-armory/utility"
-)
+import "github.com/callrua/wow-armory/cmd"
 
 func main() {
-	clientId := flag.String("id", "", "Battle.net Client ID")
-	clientSecret := flag.String("secret", "", "Battle.net Client Secret")
-	flag.Parse()
-
-	blizzClient, ctx := utility.DoAuth(clientId, clientSecret)
-	data, _, requestErr := blizzClient.WoWCharacterProfileSummary(ctx, "outland", "puzzle")
-	if requestErr != nil {
-		fmt.Println("Error querying API", requestErr)
-	}
-
-	fmt.Printf("%+v\n", data)
+	//clientId := flag.String("id", "", "Battle.net Client ID")
+	//clientSecret := flag.String("secret", "", "Battle.net Client Secret")
+	//flag.Parse()
+	//
+	//blizzClient, ctx := utility.DoAuth(clientId, clientSecret)
+	//data, _, requestErr := blizzClient.WoWCharacterProfileSummary(ctx, "outland", "puzzle")
+	//if requestErr != nil {
+	//	fmt.Println("Error querying API", requestErr)
+	//}
+	//
+	//fmt.Printf("%+v\n", data)
+	cmd.Execute()
 }
